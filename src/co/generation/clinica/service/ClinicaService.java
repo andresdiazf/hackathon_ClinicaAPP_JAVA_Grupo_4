1dfa5f2422c213f9f5f84b147334b7c72a809366
JhonyDMoreno <jhonydavidmoreno@gmail.com>
2026-05-08 16:50:43 -0500

    Mis cambios para la hackathon agregado turno

--- src/co/generation/clinica/service/ClinicaService.java
+++ src/co/generation/clinica/service/ClinicaService.java
@@ -1,9 +1,19 @@
 package co.generation.clinica.service;
+
+import co.generation.clinica.model.EstadoTurno;
+import co.generation.clinica.model.Turno;
+import co.generation.clinica.model.Medico;
+import co.generation.clinica.model.Paciente;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Comparator;
 
 public class ClinicaService {
+    private List <Paciente> pacientes = new ArrayList<>();
+    private List <Medico> medicos = new ArrayList<>();
+    private List <Turno> turnos = new ArrayList<>();
+
     public void registrarMedico(Medico m){
         if (!m.esValido()){
             System.out.println("Medico no valido. Verifique los datos");
@@ -52,6 +62,60 @@ public class ClinicaService {
             System.out.println(medico);
 
         }
+    }
+
+    public void asignarTurno (Turno t){
+        Paciente paciente = buscarPorCedula(t.getPaciente().getCedula());
+        if (paciente == null){
+            System.out.println("Paciente no encontrado");
+            return;
+        }
+        Medico medico = buscarPorNombreApellido(t.getMedico().getNombre(),t.getMedico().getApellido());
+        if (medico == null){
+            System.out.println("Medico no encontrado");
+            return;
+        }
+        if (turnos.contains(t)){
+            System.out.println("Ya existe un turno con ese medico en esa fecha y hora");
+            return;
+        }
+
+        int nuevoId;
+        if (turnos.isEmpty()){
+            nuevoId = 1;
+        } else {
+            int maxId = 0;
+            for (Turno turno: turnos){
+                if (turno.getId() > maxId){
+                    maxId = turno.getId();
+
+                }
+            }
+            nuevoId = maxId +1;
+        }
+        t.setId(nuevoId);
+        turnos.add(t);
+        System.out.println("Turno registrado: "+ t);
+
+    }
+    public void cancelarTurno (int idTurno){
+        Turno encontrado = null;
+        for (Turno turno : turnos){
+            if (turno.getId()== idTurno){
+                encontrado=turno;
+            }
+        }
+        if (encontrado == null ){
+            System.out.println("El turno no existe");
+            return;
+        }
+        if (encontrado.getEstado () == EstadoTurno.ATENDIDO || encontrado.getEstado() == EstadoTurno.CANCELADO) {
+            System.out.println("El turno no se puede cancelar");
+            return;
+        }
+        encontrado.setEstado(EstadoTurno.CANCELADO);
+        System.out.println("Turno cancelado: " + encontrado);
+
     }
 
 }
